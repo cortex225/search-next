0123da57437018814035c380ab0b63074f295867
jlgouaho <1900751@CegepGranby.qc.ca>
2024-05-28 09:39:09 -0400

    Update page.tsx

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
-import React from "react";
+import React, { Suspense } from "react";
 import { db } from "../db";
 import { Product, productsTable } from "../db/schema";
 import { sql } from "drizzle-orm";
@@ -8,6 +8,7 @@ import Link from "next/link";
 import { Index } from "@upstash/vector";
 import { vectorize } from "@/lib/vectorize";
 import { X } from "lucide-react";
+import punycode from 'punycode';
 
 interface PageProps {
   searchParams: {
@@ -19,9 +20,7 @@ export type CoreProduct = Omit<Product, "createdAt" | "updatedAt">;
 
 const index = new Index<CoreProduct>();
 
-const page = async ({ searchParams }: PageProps) => {
-  const query = searchParams.query;
-
+const page = async ({ searchParams: { query } }: PageProps) => {
   if (Array.isArray(query) || !query) {
     return redirect("/");
   }
@@ -79,37 +78,40 @@ const page = async ({ searchParams }: PageProps) => {
   }
 
   return (
-    <ul className="py-4 divide-y divide-zinc-100 bg-white shadow-md rounded-b-md">
-      {products.slice(0, 3).map((product) => (
-        <Link key={product.id} href={`/products/${product.id}`}>
-          <li className="mx-auto py-4 px-8 flex space-x-4">
-            <div className="relative flex items-center bg-zinc-100 rounded-lg h-40 w-40">
-              <Image
-                loading="eager"
-                fill
-                alt="product-image"
-                src={`/${product.imageId}`}
-              />
-            </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <ul className="py-4 divide-y divide-zinc-100 bg-white shadow-md rounded-b-md">
+        {products.slice(0, 3).map((product) => (
+          <Link key={product.id} href={`/products/${product.id}`}>
+            <li className="mx-auto py-4 px-8 flex space-x-4">
+              <div className="relative flex items-center bg-zinc-100 rounded-lg h-40 w-40">
+                <Image
+                  loading="eager"
+                  fill
+                  alt="product-image"
+                  src={`/${product.imageId}`}
+                />
+              </div>
 
-            <div className="w-full flex-1 space-y-2 py-1">
-              <h1 className="text-lg font-medium text-gray-900">
-                {product.name}
-              </h1>
+              <div className="w-full flex-1 space-y-2 py-1">
+                <h1 className="text-lg font-medium text-gray-900">
+                  {product.name}
+                </h1>
 
-              <p className="prose prose-sm text-gray-500 line-clamp-3">
-                {product.description}
-              </p>
+                <p className="prose prose-sm text-gray-500 line-clamp-3">
+                  {product.description}
+                </p>
 
-              <p className="text-base font-medium text-gray-900">
-                ${product.price.toFixed(2)}
-              </p>
-            </div>
-          </li>
-        </Link>
-      ))}
-    </ul>
+                <p className="text-base font-medium text-gray-900">
+                  ${product.price.toFixed(2)}
+                </p>
+              </div>
+            </li>
+          </Link>
+        ))}
+      </ul>
+    </Suspense>
   );
 };
 
 export default page;
+
